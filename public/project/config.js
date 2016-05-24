ddaf4798078d4cfd35ad6ebebc19b1f27e85b494
Austin Colcord <ajdcolcord@gmail.com>
2016-05-23 21:40:03 -0400

    Added angular routing to all pages. Moved all view files under new views directory

---
+++ public/project/config.js
@@ -0,0 +1,44 @@
+(function() {
+    angular
+        .module("Prepster")
+        .config(Config);
+    
+    function Config($routeProvider) {
+        $routeProvider
+            .when("/", {
+                templateUrl: "views/user/login.view.client.html"
+            })
+            .when("/login", {
+                templateUrl: "views/user/login.view.client.html"
+            })
+            .when("/register", {
+                templateUrl: "views/user/register.view.client.html"
+            })
+            .when("/user/:uid", {
+                templateUrl: "views/user/employee-profile.view.client.html"
+            })
+            .when("/user/:uid/edit", {
+                templateUrl: "views/user/employee-profile-edit.view.client.html"
+            })
+            .when("/user/:uid/prep/prep-list", {
+                templateUrl: "views/prep/prep-list.view.client.html"
+            })
+            .when("/user/:uid/prep/timer-list", {
+                templateUrl: "views/timer/timer-list.view.client.html"
+            })
+            .when("/user/:uid/recipe/recipe-book", {
+                templateUrl: "views/recipe/recipe-book.view.client.html"
+            })
+            .when("/user/:uid/recipe/:rid", {
+                templateUrl: "views/recipe/recipe-view.view.client.html"
+            })
+            .when("/user/:uid/recipe/:rid/edit", {
+            templateUrl: "views/recipe/recipe-edit.view.client.html"
+            })
+            .when("/user/:uid/recipe/new", {
+                templateUrl: "views/recipe/recipe-new.view.client.html"
+            })
+
+            .otherwise({redirectTo: "views/user/login.view.client.html"})
+    }
+})();
