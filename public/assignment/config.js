2927b327bee17e0075ca8f606c480b65841aa6e8
Austin Colcord <ajdcolcord@gmail.com>
2016-05-24 14:28:31 -0400

    Created user login controller, added error messages for login page when user not found.

--- public/assignment/config.js
+++ public/assignment/config.js
@@ -6,10 +6,13 @@
     function Config($routeProvider) {
         $routeProvider
             .when("/", {
-                templateUrl: "views/user/login.view.client.html"
+                templateUrl: "views/user/login.view.client.html",
+                controller: "LoginController"
             })
             .when("/login", {
-                templateUrl: "views/user/login.view.client.html"
+                templateUrl: "views/user/login.view.client.html",
+                controller: "LoginController",
+                controllerAs: "model"
             })
             .when("/register", {
                 templateUrl: "views/user/register.view.client.html"
