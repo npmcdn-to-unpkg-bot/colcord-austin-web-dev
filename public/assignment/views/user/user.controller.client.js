d15f794161ae9f576a8f3c24ebe752a00d2c4100
Austin Colcord <ajdcolcord@gmail.com>
2016-05-24 18:39:33 -0400

    Added register controller

--- public/assignment/views/user/user.controller.client.js
+++ public/assignment/views/user/user.controller.client.js
@@ -2,7 +2,8 @@
     angular
         .module("WebAppMaker")
         .controller("LoginController", LoginController)
-        .controller("ProfileController", ProfileController);
+        .controller("ProfileController", ProfileController)
+        .controller("RegisterController", RegisterController);
 
 
     var users = [
@@ -26,6 +27,41 @@
                 else {
                     vm.error = "User not found";
                 }
+            }
+        }
+    }
+
+    function RegisterController($location) {
+        var vm = this;
+
+        vm.register = register;
+        function register(username, password, verifypassword) {
+            var taken = false;
+
+            for(var i in users) {
+                if (users[i].username === username) {
+                    vm.error = "Username taken";
+                    taken = true;
+                }
+            }
+            if (!taken) {
+                if (password !== verifypassword) {
+                    vm.error = "Passwords do not match";
+                }
+                else {
+                    users.push({
+                        _id: '999',
+                        username: username,
+                        password: password,
+                        firstName: '',
+                        lastName: ''
+                    });
+                    $location.url("/user/" + 999);
+                    console.log(username)
+                }
+            }
+            else {
+                vm.error = "Username taken";
             }
         }
     }
