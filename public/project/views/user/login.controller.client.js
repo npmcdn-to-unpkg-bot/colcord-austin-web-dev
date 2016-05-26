5d9ef4e45fc60298e89d36234360e8791aec4d2d
Austin Colcord <ajdcolcord@gmail.com>
2016-05-26 10:57:43 -0400

    Adding in login controller

---
+++ public/project/views/user/login.controller.client.js
@@ -0,0 +1,23 @@
+(function() {
+    angular
+        .module("Prepster")
+        .controller("LoginController", LoginController);
+
+    function LoginController($location, UserService) {
+        var vm = this;
+
+        vm.login = login;
+        function login(username, password) {
+            var user = UserService.findUserByCredentials(username, password);
+
+            if(user) {
+                var id = user._id;
+                $location.url("/user/" + id);
+            }
+            else {
+                vm.error = "User not found";
+            }
+        }
+    }
+    
+})();
