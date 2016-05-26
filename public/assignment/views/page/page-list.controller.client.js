7e00ea8870f4214b375083d4bf0dd94996d26494
Austin Colcord <ajdcolcord@gmail.com>
2016-05-25 20:38:59 -0400

    Added page-list.controller, removed page.controller, page-list now dynamic

---
+++ public/assignment/views/page/page-list.controller.client.js
@@ -0,0 +1,15 @@
+(function() {
+    angular
+        .module("WebAppMaker")
+        .controller("PageListController", PageListController);
+    
+    function PageListController($routeParams, PageService) {
+        var vm = this;
+        vm.uid = $routeParams.uid;
+        vm.wid = $routeParams.wid;
+        function init() {
+            vm.pages = PageService.findPageByWebsiteId(vm.wid);
+        }
+        init();
+    }
+})();
