967a9e9a008a7c56919984d818be80b596c034be
Austin Colcord <ajdcolcord@gmail.com>
2016-05-28 09:05:14 -0400

    Adding 'add to prep list' for recipe book

--- public/project/views/recipe/recipe-book.controller.client.js
+++ public/project/views/recipe/recipe-book.controller.client.js
@@ -3,16 +3,28 @@
         .module("Prepster")
         .controller("RecipeBookController", RecipeBookController);
 
-    function RecipeBookController($routeParams, RecipeService, UserService) {
+    function RecipeBookController($routeParams, RecipeService, UserService, PrepService) {
         var vm = this;
+        vm.addToPrepToDo = addToPrepToDo;
         
         vm.uid = $routeParams["uid"];
         
         function init() {
             vm.user = UserService.findUserById(vm.uid);
             vm.recipeBook = RecipeService.findRecipesByRestaurant(vm.user.restaurantId);
+            vm.prepList = PrepService.findPrepListByRestaurantId(vm.user.restaurantId);
+
         }
         init();
+
+        function addToPrepToDo(recipeId) {
+            var newPrepItem = {
+                recipeId: recipeId,
+                important: false,
+                signer: "",
+                timeStamp: (new Date).toDateString()};
+            PrepService.addToPrepList(vm.prepList._id, newPrepItem);
+        }
     }
     
 })();
