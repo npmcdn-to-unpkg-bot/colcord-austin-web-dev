7c719fa69b554b2791dba3d72e49a3264c86a2ae
Austin Colcord <ajdcolcord@gmail.com>
2016-05-29 16:39:37 -0400

    Adding form validation for page-new and website-new names

--- public/assignment/views/page/page-new.controller.client.js
+++ public/assignment/views/page/page-new.controller.client.js
@@ -11,16 +11,20 @@
         vm.wid = $routeParams["wid"];
 
         function createPage(name, title) {
-            var id = (new Date).getTime();
-
-            var newPage = {
-                _id: id,
-                name: name,
-                websiteId: vm.wid,
-                title: title
-            };
-            PageService.createPage(vm.wid, newPage);
-            $location.url("/user/" + vm.uid + "/website/" + vm.wid + "/page");
+            if (name != null) {
+                var id = (new Date).getTime();
+                var newPage = {
+                    _id: id,
+                    name: name,
+                    websiteId: vm.wid,
+                    title: title
+                };
+                PageService.createPage(vm.wid, newPage);
+                $location.url("/user/" + vm.uid + "/website/" + vm.wid + "/page");
+            }
+            else {
+                vm.error = "Please Enter a Page Name";
+            }
         }
 
     }
--- public/assignment/views/website/website-new.controller.client.js
+++ public/assignment/views/website/website-new.controller.client.js
@@ -11,17 +11,22 @@
 
 
         function createWebsite(name, description) {
-            var id = (new Date).getTime();
+            if (name != null) {
+                var id = (new Date).getTime();
 
-            var newWebsite = {
-                _id: id,
-                name: name,
-                developerId: vm.uid,
-                description: description
-            };
-            
-            WebsiteService.createWebsite(vm.uid, newWebsite);
-            $location.url("/user/" + vm.uid + "/website");
+                var newWebsite = {
+                    _id: id,
+                    name: name,
+                    developerId: vm.uid,
+                    description: description
+                };
+
+                WebsiteService.createWebsite(vm.uid, newWebsite);
+                $location.url("/user/" + vm.uid + "/website");
+            }
+            else {
+                vm.error = "Please Enter a Website Name";
+            }
         }
 
     }
