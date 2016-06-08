3911c93f4d38aca2627649ac9fa148ec79e28b94
Austin Colcord <ajdcolcord@gmail.com>
2016-06-08 17:20:20 -0400

    Changing server setup for openshift. Adding back in description into pageSchema. Setting width and height to strings for widgetSchema, default values to 100% width, auto height. Adding Text widget and HTML widgets with textAngular,

--- server.js
+++ server.js
@@ -1,39 +1,34 @@
 var express = require('express');
 var app = express();
+// var mongoose = require('mongoose');
+//
+// var url = '127.0.0.1:27017/webdev' + process.env.OPENSHIFT_APP_NAME;
+
 var mongoose = require('mongoose');
+var connectionString = 'mongodb://127.0.0.1:27017/webdev';
 
-var url = '127.0.0.1:27017/' + process.env.OPENSHIFT_APP_NAME;
-
-// if OPENSHIFT env variables are present, use the available connection info:
-if (process.env.OPENSHIFT_MONGODB_DB_URL) {
-    url = process.env.OPENSHIFT_MONGODB_DB_URL +
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
         process.env.OPENSHIFT_APP_NAME;
 }
+mongoose.connect(connectionString);
 
-//NEW TEST MONGOOSE CONNECTION
-// if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-//     connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-//         process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-//         process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-//         process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-//         process.env.OPENSHIFT_APP_NAME;
-// }
+// // Connect to mongodb
+// var connect = function () {
+//     mongoose.connect(url);
+// };
+// connect();
 
-// mongoose.connect('mongodb://localhost/cs4550summer1');
+// var db = mongoose.connection;
 
-// Connect to mongodb
-var connect = function () {
-    mongoose.connect(url);
-};
-connect();
-
-var db = mongoose.connection;
-
-db.on('error', function(error){
-    console.log("Error loading the db - "+ error);
-});
-
-db.on('disconnected', connect);
+// db.on('error', function(error){
+//     console.log("Error loading the db - "+ error);
+// });
+//
+// db.on('disconnected', connect);
 //////////////////////////////
 
 var bodyParser = require('body-parser');
