e750c5dbf297489892fa993e9037952f25dbc6c8
Austin Colcord <ajdcolcord@gmail.com>
2016-06-09 17:58:04 -0400

    form-validation with feedback on website new

--- public/assignment/views/website/website-new.controller.client.js
+++ public/assignment/views/website/website-new.controller.client.js
@@ -6,10 +6,12 @@
     function NewWebsiteController($location, $routeParams, WebsiteService) {
         var vm = this;
         vm.createWebsite = createWebsite;
+        vm.submitted = false;
 
         vm.uid = $routeParams["uid"];
       
         function createWebsite(name, description) {
+            vm.submitted = true;
             if (name != null && name != "") {
                 var id = (new Date).getTime();
       
@@ -25,6 +27,7 @@
                         function(response) {
                             vm.success = "Created new website";
                             $location.url("/user/" + vm.uid + "/website");
+                            vm.submitted = false;
                         },
                         function(error) {
                             vm.error = error.data;
