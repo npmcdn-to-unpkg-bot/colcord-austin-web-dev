7e9ec0befb511f37386d681517c4affca530523b
Austin Colcord <ajdcolcord@gmail.com>
2016-06-09 18:06:35 -0400

    form-validation with feedback on page-new

--- public/assignment/views/page/page-new.controller.client.js
+++ public/assignment/views/page/page-new.controller.client.js
@@ -6,11 +6,13 @@
     function NewPageController($location, $routeParams, PageService) {
         var vm = this;
         vm.createPage = createPage;
+        vm.submitted = false;
 
         vm.uid = $routeParams["uid"];
         vm.wid = $routeParams["wid"];
 
         function createPage(name, title) {
+            vm.submitted = true;
             if (name != null && name != "") {
                 var newPage = {
                     name: name,
@@ -23,6 +25,7 @@
                         function(response) {
                             vm.success = "Created new website";
                             $location.url("/user/" + vm.uid + "/website/" + vm.wid + "/page");
+                            vm.submitted = false;
                         },
                         function(error) {
                             vm.error = error.data;
