addb2775694f4b5592bdaf2c399ea9d014769503
Austin Colcord <ajdcolcord@gmail.com>
2016-06-18 10:41:58 -0400

    Locking all private pages unless logged in

--- public/project/config.js
+++ public/project/config.js
@@ -34,42 +34,66 @@
             .when("/user/:uid/edit", {
                 templateUrl: "views/user/employee-profile-edit.view.client.html",
                 controller: "ProfileEditController",
-                controllerAs: "model"
+                controllerAs: "model",
+                resolve: {
+                    loggedin: checkLoggedin
+                }
             })
             .when("/user/:uid/prep/prep-list", {
                 templateUrl: "views/prep/prep-list.view.client.html",
                 controller: "PrepListController",
-                controllerAs: "model"
+                controllerAs: "model",
+                resolve: {
+                    loggedin: checkLoggedin
+                }
             })
             .when("/user/:uid/prep/timer-list", {
                 templateUrl: "views/timer/timer-list.view.client.html",
                 controller: "TimerListController",
-                controllerAs: "model"
+                controllerAs: "model",
+                resolve: {
+                    loggedin: checkLoggedin
+                }
             })
             .when("/user/:uid/recipe/new", {
                 templateUrl: "views/recipe/recipe-new.view.client.html",
                 controller: "RecipeNewController",
-                controllerAs: "model"
+                controllerAs: "model",
+                resolve: {
+                    loggedin: checkLoggedin
+                }
             })
             .when("/user/:uid/recipe/recipe-book", {
                 templateUrl: "views/recipe/recipe-book.view.client.html",
                 controller: "RecipeBookController",
-                controllerAs: "model"
+                controllerAs: "model",
+                resolve: {
+                    loggedin: checkLoggedin
+                }
             })
             .when("/user/:uid/recipe/:rid", {
                 templateUrl: "views/recipe/recipe-view.view.client.html",
                 controller: "RecipeViewController",
-                controllerAs: "model"
+                controllerAs: "model",
+                resolve: {
+                    loggedin: checkLoggedin
+                }
             })
             .when("/user/:uid/recipe/:rid/edit", {
                 templateUrl: "views/recipe/recipe-edit.view.client.html",
                 controller: "RecipeEditController",
-                controllerAs: "model"
+                controllerAs: "model",
+                resolve: {
+                    loggedin: checkLoggedin
+                }
             })
             .when("/user/:uid/recipe/new/search", {
                 templateUrl: "views/recipe/recipe-search.view.client.html",
                 controller: "RecipeSearchController",
-                controllerAs: "model"
+                controllerAs: "model",
+                resolve: {
+                    loggedin: checkLoggedin
+                }
             })
 
             //default route - login
