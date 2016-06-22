7480ab7bf83552a0bec0b652b69ad41968802f71
Austin Colcord <ajdcolcord@gmail.com>
2016-06-22 13:18:55 -0400

    Adding in ejs examples from class - now working again (had lost some steps, and wasn't working before)

--- lectures/app.js
+++ lectures/app.js
@@ -1,30 +1,29 @@
 module.exports = function(app) {
-    app.get('/lectures/hello/:name', helloHandler);
+
     app.get('/lectures/script', scriptHandler);
-    
+    app.get('/lectures/hello/:name', helloHandler);
+
     var script = [
         {type: 'string'},
-        {type: 'boolean'},
         {type: 'string'},
-        {type: 'date'},
         {type: 'numeric'},
-        {type: 'string'},
+        {type: 'boolean'},
+        {type: 'date'},
         {type: 'numeric'}
     ];
-    
-    function helloHandler(req, res) {
-        var name = req.params.name;
-        console.log(name);
-        var data = {
-            name: name
-        };
-        res.render("lectures/hello", data);
-    }
-    
+
     function scriptHandler(req, res) {
         var data = {
             script: script
         };
         res.render('lectures/script/script', data);
+    }
+
+    function helloHandler(req, res) {
+        var name = req.params.name;
+        var data = {
+            name: name
+        };
+        res.render('lectures/hello', data);
     }
 };
--- server.js
+++ server.js
@@ -8,9 +8,11 @@ var passport = require('passport');
 
 var connectionString = 'mongodb://127.0.0.1:27017/webdev';
 
-// example app for ejs
+// example app for ejs/wam scripts from lectures
 app.set('view engine', 'ejs');
-require('./lectures/app')(app);
+require('./lectures/app.js')(app);
+require('./experiments/hello.js')(app);
+require('./wam/app')(app);
 //////////////////////
 
 var assignment = require('./assignment/app.js');
